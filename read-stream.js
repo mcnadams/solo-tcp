f648b0de77b0c4eb1022947e1c30f4207728a041
mcnadams <mcneilbo@oregonstate.edu>
2019-04-10 09:22:19 -0700

    read stream

--- read-stream.js
+++ read-stream.js
@@ -1,3 +1,16 @@
 const fs = require('fs');
 
 const rs = fs.createReadStream('./1_streams.md');
+
+rs.on('data', chunk => {
+    console.log(chunk);
+});
+
+let body = '';
+rs.on('data', chunk => {
+    body += chunk;
+});
+
+rs.on('end', () => {
+    console.log('body:', body);
+});
