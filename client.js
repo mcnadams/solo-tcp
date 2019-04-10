9fa2f2733a7d02821c2ebd651ccaf37e4bc50156
mcnadams <mcneilbo@oregonstate.edu>
2019-04-10 16:42:31 -0700

    create prompt

--- client.js
+++ client.js
@@ -1,6 +1,15 @@
 const net = require('net');
+const readline = require('readline');
 
-const client = net.createConnection(12345, 'localhost', () => {
-    console.log('I am connected');
-    client.write('hello I am a client');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '> '
 });
+
+rl.prompt();
+
+// const client = net.createConnection(12345, 'localhost', () => {
+//     console.log('I am connected');
+//     client.write('hello I am a client');
+// });
